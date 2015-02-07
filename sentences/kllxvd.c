fb369de57a9d06cc45846f0c82afcd3e8d2f1dfd
lundstrom25 <kllxvd@mail.missouri.edu>
2015-02-07 00:16:42 -0600

    something

---
+++ sentences/kllxvd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int kllxvd()
+{
+	printf("\n'Bring us the one who messed up this assignment' the ninjas demanded 'we shall make them pay'");
+	return 0; 
+}
+
+int kllxvd2()
+{
+	printf("\n So the class all turned in the guy who had added broken code to the last exercise and the ninjas took him away.\n");
+	return 0;
+}
